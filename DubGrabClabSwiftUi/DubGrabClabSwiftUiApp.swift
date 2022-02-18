de66015e704b14eaae33515c1e71ac9cfa956fd1
Hiroki Kondo <kondo.matr02@gmail.com>
2022-02-18 14:46:14 +0900

    Initial Commit

---
+++ DubGrabClabSwiftUi/DubGrabClabSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubGrabClabSwiftUiApp.swift
+//  DubGrabClabSwiftUi
+//
+//  Created by 近藤宏輝 on 2022/02/18.
+//
+
+import SwiftUI
+
+@main
+struct DubGrabClabSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
