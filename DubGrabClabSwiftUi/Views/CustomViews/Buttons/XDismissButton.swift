cba9e3a5c9b1af867a458e0fb6a4ee17f163f3d2
Hiroki Kondo <kondo.matr02@gmail.com>
2022-02-19 17:49:24 +0900

    Working with DDGProfile - OnboardViewを実装

---
+++ DubGrabClabSwiftUi/Views/CustomViews/Buttons/XDismissButton.swift
@@ -0,0 +1,28 @@
+//
+//  XDismissButton.swift
+//  DubGrabClabSwiftUi
+//
+//  Created by 近藤宏輝 on 2022/02/19.
+//
+
+import SwiftUI
+
+struct XDismissButton: View {
+    var body: some View {
+        ZStack {
+            Circle()
+                .frame(width: 30, height: 30)
+                .foregroundColor(.brandPrimary)
+            Image(systemName: "xmark")
+                .foregroundColor(.white)
+                .imageScale(.small)
+                .frame(width: 44, height: 44)
+        }
+    }
+}
+
+struct XDismissButton_Previews: PreviewProvider {
+    static var previews: some View {
+        XDismissButton()
+    }
+}
---
+++ DubGrabClabSwiftUi/Views/CustomViews/LogoView.swift
@@ -0,0 +1,26 @@
+//
+//  LogoView.swift
+//  DubGrabClabSwiftUi
+//
+//  Created by 近藤宏輝 on 2022/02/19.
+//
+
+import SwiftUI
+
+struct LogoView: View {
+    
+    var frameWidth: CGFloat
+    
+    var body: some View {
+        Image("ddg-map-logo")
+            .resizable()
+            .scaledToFit()
+            .frame(width: frameWidth)
+    }
+}
+
+struct LogoView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogoView(frameWidth: 250)
+    }
+}
