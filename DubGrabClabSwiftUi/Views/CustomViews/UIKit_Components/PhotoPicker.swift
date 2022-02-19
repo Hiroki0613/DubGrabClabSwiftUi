89442a55ee6183af6993744efaf183d15e0988f3
Hiroki Kondo <kondo.matr02@gmail.com>
2022-02-19 19:56:49 +0900

    Working with DDGProfile - convertToCKAssetを実装

--- DubGrabClabSwiftUi/Views/CustomViews/UIKit_Components/PhotoPicker.swift
+++ DubGrabClabSwiftUi/Views/CustomViews/UIKit_Components/PhotoPicker.swift
@@ -35,8 +35,8 @@ struct PhotoPicker: UIViewControllerRepresentable {
         
         func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
             if let image = info[.editedImage] as? UIImage {
-                let compressedImageData = image.jpegData(compressionQuality: 0.1)!
-                photoPicker.image = UIImage(data: compressedImageData)!
+//                let compressedImageData = image.jpegData(compressionQuality: 0.1)!
+                photoPicker.image = image
             }
             
             photoPicker.presentationMode.wrappedValue.dismiss()
